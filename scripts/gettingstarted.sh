6f035fdaa0d2e6dfc59b7ad1d9ac1e5db2e88c56
Jörg Kantel <joerg@kantel.de>
2021-11-23 18:01:19 +0100

    test

--- scripts/gettingstarted.sh
+++ scripts/gettingstarted.sh
@@ -1,8 +1,10 @@
 #!/usr/local/bin/gnuplot
 
+set terminal gif
+set output "gnuplottest.gif"
 plot sin(1/x)*cos(x)
 
-set terminal pngcairo size 900, 600 enhanced font "Verdana, 14"
-set output "gnuplottest.png"
+# set terminal pngcairo size 900, 600 enhanced font "Verdana, 14"
+# set output "gnuplottest.png"
 
-replot
+# replot
